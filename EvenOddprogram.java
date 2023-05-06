d440994a122ac2ceed5579e260e0ce9dc6c7676f
Mihirkyada1111 <130635496+Mihirkyada1111@users.noreply.github.com>
2023-05-06 11:34:19 +0530

    Add files via upload

---
+++ EvenOddprogram.java
@@ -0,0 +1,12 @@
+public class EvenOddprogram {
+    public static void main(String[] args) {
+
+        int n = 0;
+        int odd = 10, even = 19;
+        if (n % 2 == 0) {
+            System.out.println("your number is odd=" + odd);
+        } else {
+            System.out.println("your number is even=" + even);
+        }
+    }
+}
